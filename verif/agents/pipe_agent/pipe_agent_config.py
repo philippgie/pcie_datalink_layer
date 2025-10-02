d47544924840bd527f213680204e54f18bac6dec
Reece Winmond <rewin2@morgan.edu>
2025-10-02 12:39:47 -0400

    Added and filled in headers for python files

--- verif/agents/pipe_agent/pipe_agent_config.py
+++ verif/agents/pipe_agent/pipe_agent_config.py
@@ -1,3 +1,37 @@
+# ==========================================================================
+#
+#  Morgan State University
+#  Open Hardware Acceleration Lab (HAL-O)
+#
+#!  Project:   Open-Source PCIe Endpoint Controller.
+#   File:      pipe_agent_config.py
+#   Author:    HAL-O
+#   Created:   10/1/25
+#
+#!  Description:
+#!   Module implements a retry management FIFO. Stores TLPs as axis frames.
+#!   Module resets read and write pointer after every frame allowing for retransmission
+#!   as long as data is not overwritten.
+#
+#
+#   Project:
+#     This file is part of the PCIe Gen1/Gen2 Endpoint Controller project.
+#     Developed as an open-source, synthesizable Verilog RTL IP core, this
+#     project provides FPGA designers and researchers with an educational
+#     and extensible platform for high-speed interconnect design.
+#
+#   Institutional Acknowledgement:
+#    - Project oversight and research guidance provided by the CEAMLS
+#      (Center for Equitable AI & Machine Learning Systems) Director.
+#
+#   Notes:
+#     - Compliant with PCIe Base Specification (Gen1: 2.5 GT/s,
+#       Gen2: 5.0 GT/s).
+# 
+#   License: MIT License
+# 
+# ==========================================================================
+
 import pyuvm
 from pyuvm import *
 from cocotb.triggers import *
